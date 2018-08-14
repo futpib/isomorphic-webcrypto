8f96ae537d9ec50e3fd19da33393e6eb29e60070
futpib <futpib@gmail.com>
2018-08-14 22:51:49 +0300

    Fix `require.getModules` + webpack

--- src/react-native.js
+++ src/react-native.js
@@ -1,11 +1,9 @@
 let generateSecureRandom;
-if (require.getModules) {
-  const NativeModules = require('react-native').NativeModules;
-  const RNSecureRandom = NativeModules.RNSecureRandom;
-  if (RNSecureRandom && RNSecureRandom.generateSecureRandomAsBase64) {
-    generateSecureRandom = require('react-native-securerandom').generateSecureRandom;
-  }
-} 
+const NativeModules = require('react-native').NativeModules;
+const RNSecureRandom = NativeModules.RNSecureRandom;
+if (RNSecureRandom && RNSecureRandom.generateSecureRandomAsBase64) {
+  generateSecureRandom = require('react-native-securerandom').generateSecureRandom;
+}
 
 if (!generateSecureRandom) {
   console.log(`
